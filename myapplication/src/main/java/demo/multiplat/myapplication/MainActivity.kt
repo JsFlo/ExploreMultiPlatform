506ff733fed5cf781db5e2d8393e3cec9ca417ff
Jose Flores <jsflores480@gmail.com>
2018-04-16 17:47:34 -0500

    Adds a call from the Android platform type to the common module

--- myapplication/src/main/java/demo/multiplat/myapplication/MainActivity.kt
+++ myapplication/src/main/java/demo/multiplat/myapplication/MainActivity.kt
@@ -2,11 +2,15 @@ package demo.multiplat.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import demo.multiplat.commonSharedCode
+import demo.multiplat.getPlatform
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        my_text_view.text = commonSharedCode(getPlatform())
     }
 }
