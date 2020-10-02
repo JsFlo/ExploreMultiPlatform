5a184f39e48e7f34974f73fe3626b02050ff1887
mr-shoxruxbek <shohruh.maxmudov@mail.ru>
2020-10-02 18:26:22 +0500

    Migrate to AndroidX

--- myapplication/src/main/java/demo/multiplat/myapplication/MainActivity.kt
+++ myapplication/src/main/java/demo/multiplat/myapplication/MainActivity.kt
@@ -1,7 +1,7 @@
 package demo.multiplat.myapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import demo.multiplat.commonSharedCode
 import demo.multiplat.getPlatform
 import kotlinx.android.synthetic.main.activity_main.*
